fc8386a91e2c9b5c61727be662e5b44ec2e76857
Andrew Elmendorf <aelmendorf234@gmail.com>
2019-12-05 16:51:01 -0500

    none

--- ManufacturingInventory.Installer/ViewModels/MainWindowViewModel.cs
+++ ManufacturingInventory.Installer/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace ManufacturingInventory.Installer.ViewModels {
     public class MainWindowViewModel : BindableBase {
-        private string _title = "Prism Application";
+        private string _title = "Inventory Installer";
         public string Title {
             get { return _title; }
             set { SetProperty(ref _title, value); }
